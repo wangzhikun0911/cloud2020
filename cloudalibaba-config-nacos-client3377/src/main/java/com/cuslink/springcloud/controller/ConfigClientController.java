8b9d447b564df76c0355edaada2e7fa1048534ac
wangzhikun <wangzhikun0807@qq.com>
2020-07-11 16:15:19 +0800

    nacos

---
+++ cloudalibaba-config-nacos-client3377/src/main/java/com/cuslink/springcloud/controller/ConfigClientController.java
@@ -0,0 +1,24 @@
+package com.cuslink.springcloud.controller;
+
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.cloud.context.config.annotation.RefreshScope;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+/**
+ * @author wangzhikun
+ * @date 2020/7/11 0011 11:07
+ * @description
+ */
+@RestController
+@RefreshScope //支持nacos的动态刷新功能
+public class ConfigClientController {
+
+    @Value("${config.info}")
+    private String configInfo;
+
+    @GetMapping("/config/info")
+    public String getConfigInfo() {
+        return configInfo;
+    }
+}
